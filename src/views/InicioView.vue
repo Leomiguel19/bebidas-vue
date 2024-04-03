e0b5e63d774ecf3b6d0b9540010cc954916a6adf
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-02 22:05:41 -0400

    Primeros pasos mostrar resultados de la consulta

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,6 +1,17 @@
 <script setup>
+    import { useBebidasStore } from '@/stores/bebidas';
+    import Receta from '../components/Receta.vue'
 
+    const bebidas = useBebidasStore()
 </script>
 <template>
     <div class="text-6xl font-extrabold">Recetas</div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+        <Receta
+            v-for="receta in bebidas.recetas"
+            :receta="receta"
+            :key="receta.idDrink"
+        />
+    </div>
 </template>
