b1c2b077b9cbe5f0d97053495cef3e8050f425f8
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-04 22:28:53 -0400

    Mostrar texto cuando no hay favoritos

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,4 +1,4 @@
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch, onMounted, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useBebidasStore } from './bebidas'
 import { useModalStore } from './modal'
@@ -47,9 +47,12 @@ export const useFavoritosStore = defineStore('favoritos', () => {
         modal.modal = false
     }
 
+    const noFavoritos = computed(() => favoritos.value.length === 0 )
+
     return {
         handleClickFavoritos,
         favoritos,
-        existeFavorito
+        existeFavorito,
+        noFavoritos
     }
 })
