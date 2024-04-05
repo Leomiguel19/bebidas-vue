fc8c443b09bcc432dcc97244cad0525ada06f4bf
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-04 22:00:05 -0400

    Mostrar los favoritos

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -1,6 +1,17 @@
 <script setup>
+    import Receta from '@/components/Receta.vue';
+    import { useFavoritosStore } from '@/stores/favoritos'
 
+    const favoritos = useFavoritosStore()
 </script>
 <template>
     <div class="text-6xl font-extrabold">Favoritos</div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+        <Receta
+            v-for="receta in favoritos.favoritos"
+            :receta="receta"
+            :key="receta.idDrink"
+        />
+    </div>
 </template>
